adbe80ba494bfcaf775def8ea4802eb71539f2dd
Akira Nishiyama <mr081677@gmail.com>
2020-09-23 21:22:27 +0900

    amend vlog option

--- gp_scoreboard-config.cmake
+++ gp_scoreboard-config.cmake
@@ -3,12 +3,12 @@
 #  gp_scoreboard_INCLUDE_DIRS      - include directories for gp_scoreboard package.
 #  gp_scoreboard_SRC_FILES         - source files for implementation.(empty)
 #  gp_scoreboard_TESTBENCH_FILES   - testbench files for simulation.
-#  gp_scoreboard_DEFINITIONS_VLOG  - additional compile option for vlog.(empty)
+#  gp_scoreboard_DEFINITIONS_VLOG  - additional compile option for vlog.
 #  gp_scoreboard_DEPENDENCIES      - gp_scoreboard components list. use for target depends.(no dependencies)
 
 # Compute paths
 set(gp_scoreboard_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR})
 set(gp_scoreboard_SRC_FILES "") #empty. this package is for simulation only.
 set(gp_scoreboard_TESTBENCH_FILES ${CMAKE_CURRENT_LIST_DIR}/gp_scoreboard.sv)
-set(gp_scoreboard_DEFINITIONS_VLOG "")
+set(gp_scoreboard_DEFINITIONS_VLOG "-i ${gp_scoreboard_INCLUDE_DIRS}")
 set(gp_scoreboard_DEPENDENCIES "")
