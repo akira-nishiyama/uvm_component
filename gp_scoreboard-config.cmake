569df5375daf685733add60d3a851e06337db9a7
Akira Nishiyama <mr081677@gmail.com>
2020-09-23 20:21:03 +0900

    add cmake config

---
+++ gp_scoreboard-config.cmake
@@ -0,0 +1,14 @@
+# - Config file for the gp_scoreboard package
+# It defines the following variables
+#  gp_scoreboard_INCLUDE_DIRS      - include directories for gp_scoreboard package.(empty)
+#  gp_scoreboard_SRC_FILES         - source files for implementation.(empty)
+#  gp_scoreboard_TESTBENCH_FILES   - testbench files for simulation.
+#  gp_scoreboard_DEFINITIONS_VLOG  - additional compile option for vlog.(empty)
+#  gp_scoreboard_DEPENDENCIES      - gp_scoreboard components list. use for target depends.(no dependencies)
+
+# Compute paths
+set(gp_scoreboard_INCLUDE_DIRS "")
+set(gp_scoreboard_SRC_FILES "") #empty. this package is for simulation only.
+set(gp_scoreboard_TESTBENCH_FILES ${CMAKE_CURRENT_LIST_DIR}/gp_scoreboard.sv)
+set(gp_scoreboard_DEFINITIONS_VLOG "")
+set(gp_scoreboard_DEPENDENCIES "")
